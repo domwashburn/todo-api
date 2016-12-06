10cb74dd44a28caa8afb2cfd00db82f32a2d2593
Dominick Washburn <domwashburn@us.ibm.com>
2016-12-06 16:11:45 -0600

    add POST for /todos

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-	id: 1,
-	description: 'do something',
-	completed: false
-},{
-	id: 2,
-	description: 'do something else',
-	completed: false
-},{
-	id: 3,
-	description: 'do this first',
-	completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
 	res.send('Todo API root');
@@ -34,6 +26,16 @@ app.get('/todos/:id', (req, res) => {
 	} else {
 		res.status(404).send(`oops! can't find a todo with the id of ${todoID}`);
 	}
+});
+// POST /todos
+app.post('/todos', (req, res) => {
+	var body = req.body;
+	// add an ID to the request body and increment the value
+	body.id = todoNextId++;
+	// push the post body to the todos array
+	todos.push(body);
+	console.log('body.description', body.description);
+	res.json(body);
 });
 
 app.listen( PORT, () => {
