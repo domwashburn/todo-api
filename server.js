a3d439c8c02d414250ad80cfce4941eced63f3aa
Dominick Washburn <domwashburn@us.ibm.com>
2016-12-07 16:46:48 -0600

    Add postgres for production

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ var db = require('./db');
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
-// var todoNextId = 1;
 
 app.use(bodyParser.json());
 
@@ -106,4 +105,4 @@ db.sequelize.sync()
 		app.listen( PORT, () => {
 			console.log('Todo API running. Listening on port ' + PORT);
 		});
-	})
+	});
