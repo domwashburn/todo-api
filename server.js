3180a6f31b4f6483e2734437e53d60b298447622
Dominick Washburn <domwashburn@us.ibm.com>
2016-12-07 11:42:13 -0600

    add ?completed to /todos

--- server.js
+++ server.js
@@ -14,14 +14,20 @@ app.get('/', (req, res) => {
 
 // GET /todos --  get the todos collection
 app.get('/todos', (req, res) => {
-	res.json(todos);
+	var queryParams = req.query;
+	var filteredTodos = todos;
+
+	if ( queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+		filteredTodos = _.where(filteredTodos, {completed: true});
+	} else if ( queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+		filteredTodos = _.where(filteredTodos, {completed: false});
+	}
+
+	res.json(filteredTodos);
 });
 // GET /todos/:id -- get a specific todo by id.
 app.get('/todos/:id', (req, res) => {
 	var todoID = parseInt(req.params.id, 10);
-	// var matchedTodo = todos.find( todo => {
-	// 	return todo.id === todoID;
-	// });
 	var matchedTodo = _.findWhere(todos, {id: todoID});
 	if(matchedTodo) {
 		res.json(matchedTodo);
