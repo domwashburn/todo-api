c8c9e5c1d73c8e83703007b74503cc1d677be4cf
Dominick Washburn <domwashburn@us.ibm.com>
2016-12-07 11:09:00 -0600

    add PUT /todos/:id

--- server.js
+++ server.js
@@ -57,6 +57,31 @@ app.delete('/todos/:id', (req, res) => {
 	}
 })
 
+// PUT /todos/:id
+app.put('/todos/:id', (req, res) => {
+	var body = _.pick(req.body, 'description', 'completed');
+	var validAttributes = {};
+	var todoID = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoID});
+	console.log('matchedTodo', matchedTodo);
+	if (!matchedTodo) {
+		return res.status(404).send();
+	}
+
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+		validAttributes.completed = body.completed;
+	} else if (body.hasOwnProperty('completed')){
+		return res.status(400).send();
+	}
+	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+		validAttributes.description = body.description.trim();
+	} else if (body.hasOwnProperty('description')){
+		return res.status(400).send();
+	}
+	_.extend(matchedTodo, validAttributes);
+	res.json(matchedTodo);
+})
+
 app.listen( PORT, () => {
 	console.log('Todo API running. Listening on port ' + PORT);
 })
